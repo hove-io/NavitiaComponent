8a11ace84b45434873d51f69833cd7b97c6f0542
Johan ROUVE <johan.rouve@canaltp.fr>
2014-04-29 20:11:28 +0200

    add journeys alias for coverage api

---
+++ Request/Parameters/CoverageJourneysParameters.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Navitia\Component\Request\Parameters;
+
+/**
+ * Alias of JourneysParameters for coverage api
+ *
+ * @copyright (c) 2014, CANALTP
+ * @author jrouve
+ */
+class CoverageJourneysParameters extends JourneysParameters
+{
+}
