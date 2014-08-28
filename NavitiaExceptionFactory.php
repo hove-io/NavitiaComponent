ff2c679c0faeb0473b26ffc7c479e487f0ad5294
Rémy Abi-Khalil <remyabi@gmail.com>
2014-08-28 09:58:59 +0200

    Exception about time out added in NavitiaExceptionFactory.

--- NavitiaExceptionFactory.php
+++ NavitiaExceptionFactory.php
@@ -3,12 +3,13 @@
 namespace Navitia\Component;
 
 use Navitia\Component\FactoryInterface;
+use Navitia\Component\Exception\NavitiaException;
 
 class NavitiaExceptionFactory implements FactoryInterface
 {
     private $httpCodes;
     private $errorIds;
-    
+
     public function __construct()
     {
         $this->httpCodes = array(
@@ -16,6 +17,7 @@ class NavitiaExceptionFactory implements FactoryInterface
             401 => 'Navitia\Component\Exception\NavitiaUnauthorizedException',
             403 => 'Navitia\Component\Exception\NavitiaForbiddenException',
             404 => 'Navitia\Component\Exception\NavitiaNotFoundException',
+            0   => 'Navitia\Component\Exception\NavitiaNotRespondingException'
         );
         $this->errorIds = array(
             'date_out_of_bounds' => 'Navitia\Component\Exception\NotFound\DateOutOfBoundsException',
@@ -25,13 +27,19 @@ class NavitiaExceptionFactory implements FactoryInterface
             'unknown_object' => 'Navitia\Component\Exception\NotFound\UnknownObjectException',
         );
     }
-    
+
     /**
      * {@inheritDoc}
      */
     public function create($httpCode, $errorId = '', $message = '')
     {
         switch ($httpCode) {
+            case 0:
+                return new $this->httpCodes[$httpCode](
+                    'Navitia timeout',
+                    $httpCode
+                );
+                break;
             case 404:
                 return new $this->errorIds[$errorId](
                     'Navitia error message: ' . $message,
